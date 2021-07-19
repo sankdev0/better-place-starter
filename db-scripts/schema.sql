7d75655df2a38dfc66c5eebad7838a700e16719c
san k <sankdev0@gmail.com>
2021-07-20 00:51:20 +0500

    Add tables to schema db-script with One-To-Many
    1. Add tables animal, animal_type, animal_status, address, city, area

--- db-scripts/schema.sql
+++ db-scripts/schema.sql
@@ -1,6 +1,7 @@
 USE `betterplace` ;
 
 DROP TABLE IF EXISTS `betterplace`.`address`;
+DROP TABLE IF EXISTS `betterplace`.`city`;
 
 DROP TABLE IF EXISTS `betterplace`.`animal`;
 DROP TABLE IF EXISTS `betterplace`.`animal_type`;
@@ -9,35 +10,62 @@ DROP TABLE IF EXISTS `betterplace`.`animal_status`;
 DROP TABLE IF EXISTS `betterplace`.`product`;
 DROP TABLE IF EXISTS `betterplace`.`product_category`;
 
+CREATE TABLE IF NOT EXISTS `betterplace`.`city` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(80) NOT NULL,
+    `country` VARCHAR(80) NOT NULL,
+    `region` VARCHAR(80) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    INDEX(`name`)
+    )
+ENGINE=InnoDB
+AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS `betterplace`.`address` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-    `address` VARCHAR(255) NULL DEFAULT NULL,
-    `city_id` BIGINT(20) NULL DEFAULT NULL,
+    `main` BIT DEFAULT 1,
+    `address` VARCHAR(80) NOT NULL DEFAULT 'No address',
+    `city_id` BIGINT(20) NOT NULL,
     `latitude` DECIMAL(8,6) NULL DEFAULT NULL,
     `longitude` DECIMAL(9,6) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`))
+    PRIMARY KEY (`id`),
+    INDEX(`address`),
+    KEY `fk_city` (`city_id`),
+    CONSTRAINT `fk_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`)
+    )
+ENGINE=InnoDB
+AUTO_INCREMENT = 1;
+
+CREATE TABLE IF NOT EXISTS `betterplace`.`area` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `main` BIT DEFAULT 1,
+    `latitude` DECIMAL(8,6) NULL DEFAULT NULL,
+    `longitude` DECIMAL(9,6) NULL DEFAULT NULL,
+    `radius` DECIMAL(9,6) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+    )
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS `betterplace`.`animal_type` (
   `id` INT(4) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(80) NULL DEFAULT NULL,
+  `name` VARCHAR(80) NOT NULL DEFAULT 'unknown',
   PRIMARY KEY (`id`),
-  INDEX(`name`))
+  INDEX(`name`)
+  )
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS `betterplace`.`animal_status` (
   `id` INT(2) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(80) NULL DEFAULT NULL,
+  `name` VARCHAR(80) NOT NULL DEFAULT 'unknown',
   PRIMARY KEY (`id`))
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS `betterplace`.`animal` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) DEFAULT NULL,
+  `name` VARCHAR(255) NOT NULL DEFAULT 'Beast',
   `birth_date` DATE,
   `description` VARCHAR(255) DEFAULT NULL,
   `image_url` VARCHAR(255) DEFAULT NULL,
@@ -46,12 +74,15 @@ CREATE TABLE IF NOT EXISTS `betterplace`.`animal` (
   `last_updated` DATETIME(6) DEFAULT NULL,
   `type_id` INT(4) NOT NULL,
   `status_id` INT(2) DEFAULT NULL,
+  `address_id` BIGINT(20) DEFAULT NULL,
   INDEX(`name`),
   PRIMARY KEY (`id`),
   KEY `fk_type` (`type_id`),
   CONSTRAINT `fk_type` FOREIGN KEY (`type_id`) REFERENCES `animal_type` (`id`),
   KEY `fk_status` (`status_id`),
-  CONSTRAINT `fk_status` FOREIGN KEY (`status_id`) REFERENCES `animal_status` (`id`)
+  CONSTRAINT `fk_status` FOREIGN KEY (`status_id`) REFERENCES `animal_status` (`id`),
+  KEY `fk_address` (`address_id`),
+  CONSTRAINT `fk_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
 ) 
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
